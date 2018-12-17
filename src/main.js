19c54ede7928bb427eb903c627f1aecedab97d09
Pritam <pritam.sarkar@medlife.com>
2018-12-17 23:16:22 +0530

    Add fileUploaders to main.js

--- src/main.js
+++ src/main.js
@@ -1,16 +1,86 @@
-export class FragmentUploader {
+/**
+ * @name Customized Fragment Uploader
+ * @author Pritam Sarkar
+ */
+export class CFUploader {
     constructor() {
-        this.fileUploaders = [];
-        this.noOfFiles = 0;
+        this._fileUploaders = new Map();
+        this._files = null;
+        this._isUploading = true;
+        this._errorMessage = "";
+        // TODO: chunk size
+        // TODO: unique id generator function
+        // TODO: file type check function
+        // TODO: file size limit
+        // TODO: concurrent uploading
+        // TODO: status of all concurrent uploads
+        // TODO: total upload status: files uploaded, percentage
     }
 
     static health() {
-        console.log("FragmentUploader running in global window!");
+        console.log("CustomizedFragmentUploader running in global window!");
     }
 
+    watchDOMId(id) {
+        document.getElementById(id).addEventListener('change', e => {
+            if(this._isUploading === false) {
+                this.clearFileUploaders();
+                if(document.getElementById(id) && document.getElementById(id).files) {
+                    this._files = document.getElementById(id).files;
+                    this.addFiles(document.getElementById(id).files);
+                }
+            } else {
+                //document.getElementById(id).disabled = true;
+                this._errorMessage = "Unable to include changes in files while upload in progress";
+            }
+        })
+    }
 
+    addFiles(files) {
+        Array.from(files).forEach(file => {this.addFile(file)});
+    }
+
+    addFile(file) {
+        let uid = file.name; // TODO: unique id generation
+        let fu = new FileUploader(file, uid);
+        this._fileUploaders.set(uid, fu);
+        console.log(this.fileUploadersArray);
+    }
+
+    clearFileUploaders() {
+        this.fileUploadersArray = new Map();
+    }
+
+    get files() {
+        return this._files;
+    }
+
+    set files(files) {
+        this._files = files;
+    }
+
+    get fileUploadersArray() {
+        return this._fileUploaders;
+    }
+
+    set fileUploadersArray(fileUploaders) {
+        this._fileUploaders = fileUploaders;
+    }
+}
+
+class FileUploader {
+    constructor(file, uid) {
+        this._file = file;
+        this._uniqueID = uid;
+        this._isUploading = false;
+    }
+}
+
+class Uploader {
+    constructor() {
+
+    }
 }
 
 
-
-window.FragmentUploader = FragmentUploader;
+window.CFUploader = CFUploader;
--- test/main-bundle.js
+++ test/main-bundle.js
@@ -9,24 +9,105 @@ var _createClass = function () { function defineProperties(target, props) { for 
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var FragmentUploader = exports.FragmentUploader = function () {
-    function FragmentUploader() {
-        _classCallCheck(this, FragmentUploader);
+/**
+ * @name Customized Fragment Uploader
+ * @author Pritam Sarkar
+ */
+var CFUploader = exports.CFUploader = function () {
+    function CFUploader() {
+        _classCallCheck(this, CFUploader);
 
-        this.fileUploaders = [];
-        this.noOfFiles = 0;
+        this._fileUploaders = new Map();
+        this._files = null;
+        this._isUploading = true;
+        this._errorMessage = "";
+        // TODO: chunk size
+        // TODO: unique id generator function
+        // TODO: file type check function
+        // TODO: file size limit
+        // TODO: concurrent uploading
+        // TODO: status of all concurrent uploads
+        // TODO: total upload status: files uploaded, percentage
     }
 
-    _createClass(FragmentUploader, null, [{
-        key: "test",
-        value: function test() {
-            console.log("FragmentUploader test successful!");
+    _createClass(CFUploader, [{
+        key: "watchDOMId",
+        value: function watchDOMId(id) {
+            var _this = this;
+
+            document.getElementById(id).addEventListener('change', function (e) {
+                if (_this._isUploading === false) {
+                    _this.clearFileUploaders();
+                    if (document.getElementById(id) && document.getElementById(id).files) {
+                        _this._files = document.getElementById(id).files;
+                        _this.addFiles(document.getElementById(id).files);
+                    }
+                } else {
+                    document.getElementById(id).disabled = true;
+                    _this._errorMessage = "Unable to include changes in files while upload in progress";
+                }
+            });
+        }
+    }, {
+        key: "addFiles",
+        value: function addFiles(files) {
+            var _this2 = this;
+
+            Array.from(files).forEach(function (file) {
+                _this2.addFile(file);
+            });
+        }
+    }, {
+        key: "addFile",
+        value: function addFile(file) {
+            var uid = file.name; // TODO: unique id generation
+            var fu = new FileUploader(file, uid);
+            this._fileUploaders.set(uid, fu);
+            console.log(this.fileUploadersArray);
+        }
+    }, {
+        key: "clearFileUploaders",
+        value: function clearFileUploaders() {
+            this.fileUploadersArray = new Map();
+        }
+    }, {
+        key: "files",
+        get: function get() {
+            return this._files;
+        },
+        set: function set(files) {
+            this._files = files;
+        }
+    }, {
+        key: "fileUploadersArray",
+        get: function get() {
+            return this._fileUploaders;
+        },
+        set: function set(fileUploaders) {
+            this._fileUploaders = fileUploaders;
+        }
+    }], [{
+        key: "health",
+        value: function health() {
+            console.log("FragmentUploader running in global window!");
         }
     }]);
 
-    return FragmentUploader;
+    return CFUploader;
 }();
 
-window.FragmentUploader = FragmentUploader;
+var FileUploader = function FileUploader(file, uid) {
+    _classCallCheck(this, FileUploader);
+
+    this._file = file;
+    this._uniqueID = uid;
+    this._isUploading = false;
+};
+
+var Uploader = function Uploader() {
+    _classCallCheck(this, Uploader);
+};
+
+window.CFUploader = CFUploader;
 
 },{}]},{},[1]);
