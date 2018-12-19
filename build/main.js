52893de0e8a9521ab81ba639625e216765320eb3
Pritam <pritam.sarkar@medlife.com>
2018-12-19 17:25:51 +0530

    Add CFUploaderAPIObject and fileUploader startUploading logic

--- build/main.js
+++ build/main.js
@@ -18,7 +18,8 @@ var CFUploader = exports.CFUploader = function () {
 
         this._fileUploaders = new Map();
         this._files = null;
-        this._isUploading = true;
+        this._targetDivID = "";
+        this._isUploading = false;
         this._errorMessage = "";
         // TODO: chunk size
         // TODO: unique id generator function
@@ -27,6 +28,13 @@ var CFUploader = exports.CFUploader = function () {
         // TODO: concurrent uploading
         // TODO: status of all concurrent uploads
         // TODO: total upload status: files uploaded, percentage
+        this._uploadAPIs = {
+            "preUploadInitiation": null,
+            "postUploadCompletion": null,
+            "preFileUpload": null,
+            "fileUploadURL": null,
+            "postFileUpload": null
+        };
     }
 
     _createClass(CFUploader, [{
@@ -35,15 +43,15 @@ var CFUploader = exports.CFUploader = function () {
             var _this = this;
 
             document.getElementById(id).addEventListener('change', function (e) {
-                if (_this._isUploading === false) {
+                if (_this.isUploading === false) {
                     _this.clearFileUploaders();
+                    _this.targetDivID = id;
                     if (document.getElementById(id) && document.getElementById(id).files) {
-                        _this._files = document.getElementById(id).files;
+                        _this.files = document.getElementById(id).files;
                         _this.addFiles(document.getElementById(id).files);
                     }
                 } else {
-                    document.getElementById(id).disabled = true;
-                    _this._errorMessage = "Unable to include changes in files while upload in progress";
+                    _this.errorMessage = "Unable to include changes in files while upload in progress!";
                 }
             });
         }
@@ -60,7 +68,7 @@ var CFUploader = exports.CFUploader = function () {
         key: "addFile",
         value: function addFile(file) {
             var uid = file.name; // TODO: unique id generation
-            var fu = new FileUploader(file, uid);
+            var fu = new FileUploader(this, file, uid);
             this._fileUploaders.set(uid, fu);
             console.log(this.fileUploadersArray);
         }
@@ -68,6 +76,57 @@ var CFUploader = exports.CFUploader = function () {
         key: "clearFileUploaders",
         value: function clearFileUploaders() {
             this.fileUploadersArray = new Map();
+        }
+    }, {
+        key: "initiateUpload",
+        value: function initiateUpload() {
+            var _this3 = this;
+
+            if (this.fileUploadersArray.size === 0) {
+                this.errorMessage = "No files selected to upload!";
+            } else {
+
+                if (!!this.uploadAPIs.preUploadInitiation) {
+                    document.getElementById(this.targetDivID).disabled = true;
+                    this.uploadAPIs.preUploadInitiation.onSuccess = function (response) {
+                        _this3.fileUploadersArray.forEach(function (fileUploader) {
+                            fileUploader.startUploading();
+                        });
+                    };
+                    this.uploadAPIs.preUploadInitiation.onFailure = function (response) {
+                        document.getElementById(_this3.targetDivID).disabled = false;
+                        console.log("preUploadInitiation failed!");
+                    };
+                    this.uploadAPIs.preUploadInitiation.execute();
+                } else {
+                    document.getElementById(this.targetDivID).disabled = true;
+                    this.fileUploadersArray.forEach(function (fileUploader) {
+                        fileUploader.startUploading();
+                    });
+                }
+            }
+        }
+    }, {
+        key: "cancelUpload",
+        value: function cancelUpload() {
+            // TODO : cancel upload
+        }
+    }, {
+        key: "set_API_URL",
+        value: function set_API_URL(apiName, apiURLObject) {
+            var APIs = this.uploadAPIs;
+            if (APIs.hasOwnProperty(apiName)) {
+                APIs[apiName] = apiURLObject;
+            }
+            this.uploadAPIs = APIs;
+        }
+    }, {
+        key: "fileUploadersArray",
+        get: function get() {
+            return this._fileUploaders;
+        },
+        set: function set(fileUploaders) {
+            this._fileUploaders = fileUploaders;
         }
     }, {
         key: "files",
@@ -78,33 +137,282 @@ var CFUploader = exports.CFUploader = function () {
             this._files = files;
         }
     }, {
-        key: "fileUploadersArray",
+        key: "targetDivID",
         get: function get() {
-            return this._fileUploaders;
+            return this._targetDivID;
         },
-        set: function set(fileUploaders) {
-            this._fileUploaders = fileUploaders;
+        set: function set(id) {
+            this._targetDivID = id;
+        }
+    }, {
+        key: "isUploading",
+        get: function get() {
+            return this._isUploading;
+        },
+        set: function set(bool) {
+            var _this4 = this;
+
+            if (this.isUploading === true && bool === false) {
+                // TODO: change condition to cfUploader status = SUCCESS
+                if (!!this.uploadAPIs.postUploadCompletion) {
+                    this.uploadAPIs.postUploadCompletion.execute();
+                    this.uploadAPIs.postUploadCompletion.onSuccess = function (response) {
+                        document.getElementById(_this4.targetDivID).disabled = false;
+                    };
+                }
+            }
+            this._isUploading = !!bool;
+        }
+    }, {
+        key: "errorMessage",
+        get: function get() {
+            return this._errorMessage;
+        },
+        set: function set(error) {
+            this._errorMessage = error;
+        }
+    }, {
+        key: "uploadAPIs",
+        get: function get() {
+            return this._uploadAPIs;
+        },
+        set: function set(apis) {
+            this._uploadAPIs = apis;
         }
     }], [{
         key: "health",
         value: function health() {
-            console.log("FragmentUploader running in global window!");
+            console.log("CustomizedFragmentUploader running in global window!");
         }
     }]);
 
     return CFUploader;
 }();
 
-var FileUploader = function FileUploader(file, uid) {
-    _classCallCheck(this, FileUploader);
+var FileUploader = function () {
+    function FileUploader(cfUploader, file, uid) {
+        _classCallCheck(this, FileUploader);
 
-    this._file = file;
-    this._uniqueID = uid;
-    this._isUploading = false;
+        this._parentCFUploader = cfUploader;
+        this._file = file;
+        this._uniqueID = uid;
+        this._isFileUploading = false;
+    }
+
+    _createClass(FileUploader, [{
+        key: "startUploading",
+        value: function startUploading() {
+            var _this5 = this;
+
+            this.parentCFUploader.isUploading = true;
+            var fileUploadPromise = new Promise(function (resolve, reject) {
+                if (!!_this5.parentCFUploader.uploadAPIs.preFileUpload) {
+                    _this5.parentCFUploader.uploadAPIs.preFileUpload.onSuccess = function (response) {
+                        resolve(true);
+                    };
+                    _this5.parentCFUploader.uploadAPIs.preFileUpload.onFailure = function (response) {
+                        console.log("preFileUpload failed!");
+                        _this5.isFileUploading = false;
+                        _this5.cancelFileUpload();
+                        reject(false);
+                    };
+                    _this5.parentCFUploader.uploadAPIs.preFileUpload.execute();
+                } else {
+                    resolve(true);
+                }
+            }).then(function (result) {
+                return new Promise(function (resolve, reject) {
+                    if (!!_this5.parentCFUploader.uploadAPIs.fileUploadURL) {
+                        _this5.parentCFUploader.uploadAPIs.fileUploadURL.onSuccess = function (response) {
+                            resolve(true);
+                        };
+                        _this5.parentCFUploader.uploadAPIs.fileUploadURL.onFailure = function (response) {
+                            console.log("fileUploadURL failed!");
+                            _this5.isFileUploading = false;
+                            _this5.cancelFileUpload();
+                            reject(false);
+                        };
+                        _this5.parentCFUploader.uploadAPIs.fileUploadURL.execute();
+                    } else {
+                        resolve(true);
+                    }
+                });
+            }).then(function (result) {
+                return new Promise(function (resolve, reject) {
+                    if (!!_this5.parentCFUploader.uploadAPIs.postFileUpload) {
+                        _this5.parentCFUploader.uploadAPIs.postFileUpload.onSuccess = function (response) {
+                            resolve(true);
+                        };
+                        _this5.parentCFUploader.uploadAPIs.postFileUpload.onFailure = function (response) {
+                            console.log("postFileUpload failed!");
+                            _this5.isFileUploading = false;
+                            _this5.cancelFileUpload();
+                            reject(false);
+                        };
+                        _this5.parentCFUploader.uploadAPIs.postFileUpload.execute();
+                    } else {
+                        resolve(true);
+                    }
+                });
+            });
+        }
+    }, {
+        key: "cancelFileUpload",
+        value: function cancelFileUpload() {
+            // TODO: on cancel file upload
+        }
+    }, {
+        key: "parentCFUploader",
+        get: function get() {
+            return this._parentCFUploader;
+        },
+        set: function set(cfUploader) {
+            this._parentCFUploader = cfUploader;
+        }
+    }, {
+        key: "isFileUploading",
+        get: function get() {
+            return this._isFileUploading;
+        },
+        set: function set(bool) {
+            this._isFileUploading = !!bool;
+        }
+    }]);
+
+    return FileUploader;
+}();
+
+var FragmentUploader = function FragmentUploader() {
+    _classCallCheck(this, FragmentUploader);
 };
 
-var Uploader = function Uploader() {
-    _classCallCheck(this, Uploader);
-};
+var UploaderAPIObject = function () {
+    function UploaderAPIObject(_ref) {
+        var url = _ref["url"],
+            method = _ref["method"],
+            header = _ref["header"],
+            body = _ref["body"],
+            postSuccess = _ref["success"],
+            postFailure = _ref["failure"];
+
+        _classCallCheck(this, UploaderAPIObject);
+
+        this._url = url;
+        this._method = method;
+        this._header = !!header ? header : {};
+        this._body = body;
+        this._onSuccess = null;
+        this._onFailure = null;
+        this._postSuccess = postSuccess;
+        this._postFailure = postFailure;
+    }
+
+    _createClass(UploaderAPIObject, [{
+        key: "execute",
+        value: function execute() {
+            var _this6 = this;
+
+            var xhr = void 0;
+            if (window.XMLHttpRequest) {
+                // for modern browsers
+                xhr = new XMLHttpRequest();
+            } else {
+                // for old IE browsers
+                xhr = new ActiveXObject("Microsoft.XMLHTTP");
+            }
+
+            xhr.onreadystatechange = function (e) {
+                if (xhr.readyState === 4) {
+                    var response = xhr.responseText && JSON.parse(xhr.responseText);
+                    if (xhr.status >= 200 && xhr.status < 300) {
+                        _this6.onSuccess(response);
+                        _this6.postSuccess(response);
+                    } else {
+                        _this6.onFailure(response);
+                        _this6.postFailure(response);
+                    }
+                }
+            };
+
+            xhr.open(this.method, this.url, true);
+            Object.keys(this.header).forEach(function (key) {
+                xhr.setRequestHeader(key, _this6.header[key]);
+            });
+
+            if (this.method === "GET") {
+                xhr.send();
+            } else if (this.method === "POST" || this.method === "PUT") {
+                xhr.send(this.body);
+            }
+        }
+    }, {
+        key: "method",
+        get: function get() {
+            return this._method;
+        },
+        set: function set(value) {
+            this._method = value;
+        }
+    }, {
+        key: "body",
+        get: function get() {
+            return this._body;
+        },
+        set: function set(value) {
+            this._body = value;
+        }
+    }, {
+        key: "url",
+        get: function get() {
+            return this._url;
+        },
+        set: function set(value) {
+            this._url = value;
+        }
+    }, {
+        key: "header",
+        get: function get() {
+            return this._header;
+        },
+        set: function set(value) {
+            this._header = value;
+        }
+    }, {
+        key: "onSuccess",
+        get: function get() {
+            return this._onSuccess;
+        },
+        set: function set(value) {
+            this._onSuccess = value;
+        }
+    }, {
+        key: "onFailure",
+        get: function get() {
+            return this._onFailure;
+        },
+        set: function set(value) {
+            this._onFailure = value;
+        }
+    }, {
+        key: "postSuccess",
+        get: function get() {
+            return this._postSuccess;
+        },
+        set: function set(value) {
+            this._postSuccess = value;
+        }
+    }, {
+        key: "postFailure",
+        get: function get() {
+            return this._postFailure;
+        },
+        set: function set(value) {
+            this._postFailure = value;
+        }
+    }]);
+
+    return UploaderAPIObject;
+}();
 
 window.CFUploader = CFUploader;
+window.CFUploaderAPI = UploaderAPIObject;
--- src/main.js
+++ src/main.js
@@ -6,7 +6,8 @@ export class CFUploader {
     constructor() {
         this._fileUploaders = new Map();
         this._files = null;
-        this._isUploading = true;
+        this._targetDivID = "";
+        this._isUploading = false;
         this._errorMessage = "";
         // TODO: chunk size
         // TODO: unique id generator function
@@ -15,6 +16,13 @@ export class CFUploader {
         // TODO: concurrent uploading
         // TODO: status of all concurrent uploads
         // TODO: total upload status: files uploaded, percentage
+        this._uploadAPIs = {
+            "preUploadInitiation" : null,
+            "postUploadCompletion" : null,
+            "preFileUpload" : null,
+            "fileUploadURL" : null,
+            "postFileUpload" : null
+        };
     }
 
     static health() {
@@ -23,15 +31,15 @@ export class CFUploader {
 
     watchDOMId(id) {
         document.getElementById(id).addEventListener('change', e => {
-            if(this._isUploading === false) {
+            if(this.isUploading === false) {
                 this.clearFileUploaders();
+                this.targetDivID = id;
                 if(document.getElementById(id) && document.getElementById(id).files) {
-                    this._files = document.getElementById(id).files;
+                    this.files = document.getElementById(id).files;
                     this.addFiles(document.getElementById(id).files);
                 }
             } else {
-                //document.getElementById(id).disabled = true;
-                this._errorMessage = "Unable to include changes in files while upload in progress";
+                this.errorMessage = "Unable to include changes in files while upload in progress!";
             }
         })
     }
@@ -42,13 +50,51 @@ export class CFUploader {
 
     addFile(file) {
         let uid = file.name; // TODO: unique id generation
-        let fu = new FileUploader(file, uid);
+        let fu = new FileUploader(this, file, uid);
         this._fileUploaders.set(uid, fu);
         console.log(this.fileUploadersArray);
     }
 
     clearFileUploaders() {
         this.fileUploadersArray = new Map();
+    }
+
+    initiateUpload() {
+        if(this.fileUploadersArray.size === 0) {
+            this.errorMessage = "No files selected to upload!";
+        } else {
+
+            if(!!this.uploadAPIs.preUploadInitiation) {
+                document.getElementById(this.targetDivID).disabled = true;
+                this.uploadAPIs.preUploadInitiation.onSuccess = (response) => {
+                    this.fileUploadersArray.forEach((fileUploader) => {
+                        fileUploader.startUploading();
+                    });
+                };
+                this.uploadAPIs.preUploadInitiation.onFailure = (response) => {
+                    document.getElementById(this.targetDivID).disabled = false;
+                    console.log("preUploadInitiation failed!")
+                };
+                this.uploadAPIs.preUploadInitiation.execute();
+            } else {
+                document.getElementById(this.targetDivID).disabled = true;
+                this.fileUploadersArray.forEach((fileUploader) => {
+                    fileUploader.startUploading();
+                });
+            }
+        }
+    }
+
+    cancelUpload() {
+        // TODO : cancel upload
+    }
+
+    get fileUploadersArray() {
+        return this._fileUploaders;
+    }
+
+    set fileUploadersArray(fileUploaders) {
+        this._fileUploaders = fileUploaders;
     }
 
     get files() {
@@ -59,28 +105,257 @@ export class CFUploader {
         this._files = files;
     }
 
-    get fileUploadersArray() {
-        return this._fileUploaders;
+    get targetDivID() {
+        return this._targetDivID;
     }
 
-    set fileUploadersArray(fileUploaders) {
-        this._fileUploaders = fileUploaders;
+    set targetDivID(id) {
+        this._targetDivID = id;
+    }
+
+    get isUploading() {
+        return this._isUploading;
+    }
+
+    set isUploading(bool) {
+        if(this.isUploading === true && bool === false) {
+            // TODO: change condition to cfUploader status = SUCCESS
+            if(!!this.uploadAPIs.postUploadCompletion) {
+                this.uploadAPIs.postUploadCompletion.execute();
+                this.uploadAPIs.postUploadCompletion.onSuccess = (response) => {
+                    document.getElementById(this.targetDivID).disabled = false;
+                };
+            }
+        }
+        this._isUploading = !!bool;
+    }
+
+    get errorMessage() {
+        return this._errorMessage;
+    }
+
+    set errorMessage(error) {
+        this._errorMessage = error;
+    }
+
+    get uploadAPIs() {
+        return this._uploadAPIs;
+    }
+
+    set_API_URL(apiName, apiURLObject) {
+        let APIs = this.uploadAPIs;
+        if(APIs.hasOwnProperty(apiName)) {
+            APIs[apiName] = apiURLObject;
+        }
+        this.uploadAPIs = APIs;
+    }
+
+    set uploadAPIs(apis) {
+        this._uploadAPIs = apis;
     }
 }
 
 class FileUploader {
-    constructor(file, uid) {
+    constructor(cfUploader, file, uid) {
+        this._parentCFUploader = cfUploader;
         this._file = file;
         this._uniqueID = uid;
-        this._isUploading = false;
+        this._isFileUploading = false;
+    }
+
+    startUploading() {
+        this.parentCFUploader.isUploading = true;
+        let fileUploadPromise = new Promise((resolve, reject) => {
+            if(!!this.parentCFUploader.uploadAPIs.preFileUpload) {
+                this.parentCFUploader.uploadAPIs.preFileUpload.onSuccess = (response) => {
+                    resolve(true);
+                };
+                this.parentCFUploader.uploadAPIs.preFileUpload.onFailure = (response) => {
+                    console.log("preFileUpload failed!");
+                    this.isFileUploading = false;
+                    this.cancelFileUpload();
+                    reject(false);
+                };
+                this.parentCFUploader.uploadAPIs.preFileUpload.execute();
+            } else {
+                resolve(true);
+            }
+        }).then((result) => {
+            return new Promise((resolve, reject) => {
+                if(!!this.parentCFUploader.uploadAPIs.fileUploadURL) {
+                    this.parentCFUploader.uploadAPIs.fileUploadURL.onSuccess = (response) => {
+                        resolve(true);
+                    };
+                    this.parentCFUploader.uploadAPIs.fileUploadURL.onFailure = (response) => {
+                        console.log("fileUploadURL failed!");
+                        this.isFileUploading = false;
+                        this.cancelFileUpload();
+                        reject(false);
+                    };
+                    this.parentCFUploader.uploadAPIs.fileUploadURL.execute();
+                } else {
+                    resolve(true);
+                }
+            });
+        }).then((result) => {
+            return new Promise((resolve, reject) => {
+                if(!!this.parentCFUploader.uploadAPIs.postFileUpload) {
+                    this.parentCFUploader.uploadAPIs.postFileUpload.onSuccess = (response) => {
+                        resolve(true);
+                    };
+                    this.parentCFUploader.uploadAPIs.postFileUpload.onFailure = (response) => {
+                        console.log("postFileUpload failed!");
+                        this.isFileUploading = false;
+                        this.cancelFileUpload();
+                        reject(false);
+                    };
+                    this.parentCFUploader.uploadAPIs.postFileUpload.execute();
+                } else {
+                    resolve(true);
+                }
+            });
+        });
+    }
+
+    cancelFileUpload() {
+        // TODO: on cancel file upload
+    }
+
+    get parentCFUploader() {
+        return this._parentCFUploader;
+    }
+
+    set parentCFUploader(cfUploader) {
+        this._parentCFUploader = cfUploader;
+    }
+
+    get isFileUploading() {
+        return this._isFileUploading;
+    }
+
+    set isFileUploading(bool) {
+        this._isFileUploading = !!bool;
     }
 }
 
-class Uploader {
+class FragmentUploader {
     constructor() {
 
     }
 }
 
+class UploaderAPIObject {
+    constructor({"url": url, "method": method, "header": header, "body": body, "success": postSuccess, "failure": postFailure}) {
+        this._url = url;
+        this._method = method;
+        this._header = !!header ? header : {};
+        this._body = body;
+        this._onSuccess = null;
+        this._onFailure = null;
+        this._postSuccess = postSuccess;
+        this._postFailure = postFailure;
+    }
+
+    execute() {
+        let xhr;
+        if(window.XMLHttpRequest) {
+            // for modern browsers
+            xhr = new XMLHttpRequest();
+        } else {
+            // for old IE browsers
+            xhr = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+
+        xhr.onreadystatechange = (e) => {
+            if(xhr.readyState === 4) {
+                let response = xhr.responseText && JSON.parse(xhr.responseText);
+                if(xhr.status >= 200 && xhr.status <300) {
+                    this.onSuccess(response);
+                    this.postSuccess(response);
+                } else {
+                    this.onFailure(response);
+                    this.postFailure(response);
+                }
+            }
+        };
+
+        xhr.open(this.method, this.url, true);
+        Object.keys(this.header).forEach((key) => {
+            xhr.setRequestHeader(key, this.header[key]);
+        });
+
+        if(this.method === "GET") {
+            xhr.send();
+        } else if(this.method === "POST" || this.method === "PUT") {
+            xhr.send(this.body);
+        }
+    }
+
+    get method() {
+        return this._method;
+    }
+
+    set method(value) {
+        this._method = value;
+    }
+
+    get body() {
+        return this._body;
+    }
+
+    set body(value) {
+        this._body = value;
+    }
+
+    get url() {
+        return this._url;
+    }
+
+    set url(value) {
+        this._url = value;
+    }
+
+    get header() {
+        return this._header;
+    }
+
+    set header(value) {
+        this._header = value;
+    }
+
+    get onSuccess() {
+        return this._onSuccess;
+    }
+
+    set onSuccess(value) {
+        this._onSuccess = value;
+    }
+
+    get onFailure() {
+        return this._onFailure;
+    }
+
+    set onFailure(value) {
+        this._onFailure = value;
+    }
+
+    get postSuccess() {
+        return this._postSuccess;
+    }
+
+    set postSuccess(value) {
+        this._postSuccess = value;
+    }
+
+    get postFailure() {
+        return this._postFailure;
+    }
+
+    set postFailure(value) {
+        this._postFailure = value;
+    }
+
+}
 
 window.CFUploader = CFUploader;
+window.CFUploaderAPI = UploaderAPIObject;
